7aa11742e955b4f3a89a8fc1c42d21505c8a70b1
Taylor Jenkins <tjenkins@udel.edu>
2025-02-10 09:14:40 -0500

    wrote favorite joke

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("What is the worst thing a programmer can do? No comment");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
